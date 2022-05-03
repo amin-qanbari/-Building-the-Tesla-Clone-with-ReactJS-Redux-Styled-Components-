0de0a7c39ba307b2303c7e579d201ddf8a28e921
amin <aminqanbarii1992@gmail.com>
2022-05-03 09:11:56 -0700

    const Wrap and styles and finish today :)

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Section = () => {
   return (
-    <div>Section</div>
-  )
-}
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Oredr Online for Touchless Delivery</p>
+      </ItemText>
+    </Wrap>
+  );
+};
 
-export default Section
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url("/images/model-s.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const ItemText = styled.div`
+
+`;
