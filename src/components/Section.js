f1f413e7d2fe0b2092f3d0923c105dbf373d41b2
amin <aminqanbarii1992@gmail.com>
2022-05-06 04:39:03 -0700

    create buttons container and button group and Down Arrow and this styles

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,15 @@ const Section = () => {
         <h1>Model S</h1>
         <p>Oredr Online for Touchless Delivery</p>
       </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 };
@@ -21,8 +30,52 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical
+  align-items: center; //horizontal
 `;
 
 const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
 
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
+
+const Buttons = styled.div``;
+
+const DownArrow = styled.img`
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
 `;
